3115966fae0691602e5623a2337660d522d81aef
Matt Porter <mporter@konsulko.com>
2015-10-22 13:14:30 -0400

    example: simple: add dbus test script comments
    Signed-off-by: Matt Porter <mporter@konsulko.com>

--- example/simple/asignal.sh
+++ example/simple/asignal.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
 # Issue an ASignal signal on the bus
+# dbus-send --bus=tcp:host=10.0.0.1,port=12330 --type=signal / com.konsulko.ucdbus.Service.ASignal
 
 dbus-send					\
 		--session			\
 		--type=signal			\
 		/				\
 		com.konsulko.ucdbus.Service.ASignal
+
--- example/simple/quit.sh
+++ example/simple/quit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 # Issue a Quit method call to the com.konsulko.ucdbus.Simple service
+# dbus-send --bus=tcp:host=10.0.0.1,port=12330 --dest=com.konsulko.ucdbus.Simple --type=method_call --print-reply / com.konsulko.ucdbus.Simple.Quit
 
 dbus-send						\
 		--session				\
@@ -9,3 +10,4 @@ dbus-send						\
 		--print-reply				\
 		/					\
 		com.konsulko.ucdbus.Simple.Quit
+
